2d1f4c10b4f9945e89a6e4afe646cb83cfcc16e4
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-10-19 16:26:40 +0800

    Change config to not use redux

--- src/context/store.ts
+++ src/context/store.ts
@@ -3,7 +3,6 @@ import { createLogger } from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 
 import commonReducer from '../features/common/slice'
-import configReducer from '../features/config/slice'
 import questionBankReducer from '../features/questionBank/slice'
 import rootSaga from '../features/rootSaga'
 import userReducer from '../features/user/slice'
@@ -19,7 +18,6 @@ extraMiddlewares.push(sagaMiddleware)
 export const store = configureStore({
   reducer: {
     common: commonReducer,
-    config: configReducer,
     questionBank: questionBankReducer,
     user: userReducer,
   },
--- src/features/questionBank/sagas.ts
+++ src/features/questionBank/sagas.ts
@@ -1,7 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
 import { all, fork, put, select, takeLatest } from 'redux-saga/effects'
 
-import { store } from '../../context/store'
 import {
   Action,
   CommonSagaActions,
@@ -9,7 +8,6 @@ import {
   ServiceResponse,
 } from '../../utils/types'
 import { addLoadingTask, removeLoadingTask } from '../common/slice'
-import { getQuestionServiceBaseUrl } from '../config/selector'
 import { getFullQuestionMap } from './selectors'
 import { addCachedFullQuestion, setCategories, setQuestionsList } from './slice'
 import { Question, QuestionBankSagaActions } from './types'
@@ -60,11 +58,6 @@ function* getSelectedQuestion(action: Action<string>) {
   } finally {
     yield put(removeLoadingTask(LoadingKeys.FETCHING_SELECTED_QUESTION))
   }
-}
-
-// TODO: Replace "http://localhost:9001" with this
-function getBaseUrl(): string {
-  return getQuestionServiceBaseUrl(store.getState())
 }
 
 export function* watchGetAllQuestions() {
