87b0878e4c51492dc0f62017ecad04e72b2e1282
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-10-19 09:12:32 +0800

    Fix environment variables not available in browser

--- src/features/config/types.ts
+++ src/features/config/types.ts
@@ -1,20 +1,21 @@
 export class Config {
-  private static readonly _nodeEnvEnvVar: string = 'NODE_ENV'
-  // If the value for the "GATEWAY_BASE_URL" env var is specified, it is used as
-  // the value for any "*_BASE_URL" env var whose value is not specified.
-  private static readonly _gatewayBaseUrlEnvVar: string = 'GATEWAY_BASE_URL'
+  private static readonly _appModeEnvVar: string = 'MODE'
+  // If the value for the "VITE_GATEWAY_BASE_URL" env var is specified, it is
+  // used as the value for any "*_BASE_URL" env var whose value is not
+  // specified.
+  private static readonly _gatewayBaseUrlEnvVar: string =
+    'VITE_GATEWAY_BASE_URL'
   private static readonly _userServiceBaseUrlEnvVar: string =
-    'USER_SERVICE_BASE_URL'
+    'VITE_USER_SERVICE_BASE_URL'
   private static readonly _questionServiceBaseUrlEnvVar: string =
-    'QUESTION_SERVICE_BASE_URL'
+    'VITE_QUESTION_SERVICE_BASE_URL'
   private static readonly _matchingServiceBaseUrlEnvVar: string =
-    'MATCHING_SERVICE_BASE_URL'
+    'VITE_MATCHING_SERVICE_BASE_URL'
   private static readonly _roomServiceBaseUrlEnvVar: string =
-    'ROOM_SERVICE_BASE_URL'
+    'VITE_ROOM_SERVICE_BASE_URL'
   private static readonly _editorServiceBaseUrlEnvVar: string =
-    'EDITOR_SERVICE_BASE_URL'
+    'VITE_EDITOR_SERVICE_BASE_URL'
 
-  private static readonly _defaultIsDevEnv: boolean = false
   private static readonly _defaultUserServiceBaseUrl: string =
     'http://localhost:9000'
   private static readonly _defaultQuestionServiceBaseUrl: string =
@@ -33,9 +34,8 @@ export class Config {
   public readonly roomServiceBaseUrl: string
   public readonly editorServiceBaseUrl: string
 
-  public constructor(env: NodeJS.ProcessEnv = process.env) {
-    this.isDevEnv =
-      env[Config._nodeEnvEnvVar] === 'development' ?? Config._defaultIsDevEnv
+  public constructor(env: ImportMetaEnv = import.meta.env) {
+    this.isDevEnv = env[Config._appModeEnvVar] === 'development'
 
     this.userServiceBaseUrl =
       env[Config._userServiceBaseUrlEnvVar] ??
