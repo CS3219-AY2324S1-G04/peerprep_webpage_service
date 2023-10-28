919caff5491e8caf97c5884c31cbb516a129849f
Jedidiah Cheng <jedidiah940@gmail.com>
2023-10-28 23:28:50 +0800

    Add light/dark mode toggling for code area

--- src/features/room/components/CodeArea.tsx
+++ src/features/room/components/CodeArea.tsx
@@ -1,4 +1,6 @@
-import { dracula } from '@uiw/codemirror-theme-dracula'
+import { useColorScheme } from '@mui/joy'
+import { noctisLilac } from '@uiw/codemirror-theme-noctis-lilac'
+import { tokyoNight } from '@uiw/codemirror-theme-tokyo-night'
 import CodeMirror, { Extension } from '@uiw/react-codemirror'
 import React from 'react'
 import * as Y from 'yjs'
@@ -10,6 +12,8 @@ function CodeArea({
   text: Y.Text
   editorExtensions: Extension[]
 }) {
+
+  const { mode } = useColorScheme()
   const [value, setValue] = React.useState(text.toString())
 
   const onChange = React.useCallback(
@@ -23,8 +27,7 @@ function CodeArea({
     <div>
       <CodeMirror
         value={value}
-        height="auto"
-        theme={dracula}
+        theme={mode === 'light' ? noctisLilac : tokyoNight}
         extensions={editorExtensions}
         onChange={onChange}
       />
