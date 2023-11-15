6fc9d4e9281f76bec379ef7997759865bf5e778e
hingen <1627313-hingen@users.noreply.gitlab.com>
2023-11-15 23:57:09 +0800

    Add color interpolation for countdown slider

--- src/features/matching/components/CountdownSlider.tsx
+++ src/features/matching/components/CountdownSlider.tsx
@@ -1,10 +1,16 @@
 import { LinearProgress } from '@mui/joy'
+import Color from 'color'
 import { useEffect, useState } from 'react'
 
 import { useAppSelector } from '../../../hooks/useAppSelector'
 import useTaskSubscriber from '../../../hooks/useTaskSubscriber'
 import { LoadingKeys } from '../../../utils/types'
 import { getMatchingStartEpoch, getMatchingTimeoutEpoch } from '../selector'
+
+const startColor: Color = new Color('#CF544E')
+const endColor: Color = new Color('#88DC88')
+const startBackgroundColor: Color = new Color('#781612')
+const endBackgroundColor: Color = new Color('#51BC51')
 
 const CountdownSlider: React.FC = () => {
   const [isCheckingStatus] = useTaskSubscriber(
@@ -13,6 +19,7 @@ const CountdownSlider: React.FC = () => {
 
   const matchingStartEpoch = useAppSelector(getMatchingStartEpoch)
   const matchingTimeoutEpoch = useAppSelector(getMatchingTimeoutEpoch)
+
   const [remainingTimePercentage, setRemainingTimePercentage] = useState(100)
   const [countdownUpdateInterval, setCountdownUpdateInterval] = useState<
     NodeJS.Timeout | undefined
@@ -48,8 +55,33 @@ const CountdownSlider: React.FC = () => {
     setRemainingTimePercentage(updatedPercentage)
   }
 
+  function getColor(startColor: Color, endColor: Color): Color {
+    const red =
+      startColor.red() +
+      ((endColor.red() - startColor.red()) * remainingTimePercentage) / 100
+    const green =
+      startColor.green() +
+      ((endColor.green() - startColor.green()) * remainingTimePercentage) / 100
+    const blue =
+      startColor.blue() +
+      ((endColor.blue() - startColor.blue()) * remainingTimePercentage) / 100
+
+    return Color.rgb(red, green, blue)
+  }
+
   return (
-    <LinearProgress size="lg" determinate value={remainingTimePercentage} />
+    <LinearProgress
+      size="lg"
+      determinate
+      value={remainingTimePercentage}
+      sx={{
+        color: getColor(startColor, endColor).toString(),
+        backgroundColor: getColor(
+          startBackgroundColor,
+          endBackgroundColor,
+        ).toString(),
+      }}
+    />
   )
 }
 
